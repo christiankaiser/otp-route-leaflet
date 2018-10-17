df2bde9a9ddc213a948b189e7d3e9487f78741c6
Christian Kaiser <ck@naxio.ch>
2018-10-17 14:35:59 +0200

    Address geocoding option

--- src/script.js
+++ src/script.js
@@ -19,25 +19,79 @@ map.on('click', function(e){
   currentPointField = currentPointField == 'fromPoint' ? 'toPoint' : 'fromPoint';
 });
 
+var geocoding = null;
+
+function calculateRouteFromAddress(){
+  geocoding = 0;
+
+  var fromAddress = $('#fromAddress').val();
+  var toAddress = $('#toAddress').val();
+
+  // Geocoding
+  geocodeAddress(fromAddress);
+  geocodeAddress(toAddress, false);
+
+}
+
+function geocodeAddress(address, from=true){
+  data = {
+    format: 'json',
+    addressdetails: 1,
+    q: address,
+    limit: 1
+  };
+  $.ajax({
+    url: 'https://nominatim.openstreetmap.org/',
+    type: 'GET',
+    dataType: 'json',
+    data: data,
+    success: from == true ? processGeocodingResultFrom : processGeocodingResultTo,
+    error: geocodingError
+  });
+}
+
+function processGeocodingResultFrom(res){
+  processGeocodingResult(res, 'from');
+}
+
+function processGeocodingResultTo(res){
+  processGeocodingResult(res, 'to');
+}
+
+function processGeocodingResult(res, fld){
+  console.log('geocoding result', res);
+  var fldId = '#' + fld + 'Point';
+  $(fldId).val(res[0].lat + ',' + res[0].lon);
+  geocoding += 1;
+  if (geocoding >= 2) calculateRoute();
+}
+
+function geocodingError(err){
+  console.log('geocoding error', err);
+}
+
 function calculateRoute(){
 
   var fromPoint = $('#fromPoint').val();
   var toPoint = $('#toPoint').val();
 
+  makeRoutingQuery({
+      fromPlace: fromPoint,
+      toPlace: toPoint,
+      mode: 'BICYCLE',
+  });
+}
+
+function makeRoutingQuery(data){
   $.ajax({
     url: 'http://130.223.67.145:8080/otp/routers/ch/plan',
     type: 'GET',
     dataType: 'json',
-    data: {
-      fromPlace: fromPoint,
-      toPlace: toPoint,
-      mode: 'CAR,WALK'
-    },
+    data: data,
     success: drawRoute,
     error: calculateRouteError,
     beforeSend: setHeader
   });
-
 }
 
 function setHeader(xhr){
